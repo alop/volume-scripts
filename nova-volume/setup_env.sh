6a9a0d9e41503eaf889221047cc5af6d7d2dd050
alop <alopgeek@gmail.com>
2013-06-17 13:37:56 -0700

    added quota sync for essex

--- nova-volume/setup_env.sh
+++ nova-volume/setup_env.sh
@@ -7,6 +7,9 @@ fi
 
 NOVA_PASS=`knife data bag show --secret-file=/etc/chef/encrypted_data_bag_secret prod_dfw2_secrets nova | grep -v id | cut -d: -f2 | sed 's/^ *//g'`
 
+KEYSTONE_PASS=`knife data bag show --secret-file=/etc/chef/encrypted_data_bag_secret prod_dfw2_secrets keystone | grep -v id | cut -d: -f2 | sed 's/^ *//g'`
+
 DB_IP='o2r1-ops'
 
 SQL_CMD="mysql -u nova -p${NOVA_PASS} -h $DB_IP nova -N"
+K_SQL_CMD="mysql -u keystone -p${KEYSTONE_PASS} -h $DB_IP keystone -N"
---
+++ nova-volume/sync_quota.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [ $( whoami ) != "root" ]; then
+	echo "This script must be run as root"
+	exit 1
+fi
+
+usage() {
+	echo "USAGE: $0 project_name
+
+	Example: $0 sandbox-iad1"
+	exit 1
+}
+
+if [ -z $1 ]; then
+	usage
+fi
+
+. setup_env.sh
+
+TENANT_NAME=$1
+
+#get tenant Id from keystone
+PROJECT_ID=`$K_SQL_CMD-e "select id from tenant where name='${TENANT_NAME}'"`
+
+#TEST QUERIES
+
+COUNT=`$SQL_CMD -e "select COUNT(*) from volumes where deleted=0 and project_id='${PROJECT_ID}'"`
+echo "Actual volume usage is $COUNT"
+
+QUOTA_VOL=`$SQL_CMD -e "select in_use from quota_usages where resource='volumes' and project_id='${PROJECT_ID}'"`
+echo "Quota thinks it's $QUOTA_VOL"
+# SQL pseudo
+
+if [ $COUNT -eq $QUOTA_VOL ]
+then
+	echo "Quota matches, exiting"
+	exit
+fi
+
+echo "Updating..."
+
+$SQL_CMD -e "update quota_usages set in_use=(select COUNT(*) from volumes where project_id='${PROJECT_ID}' and deleted=0) where project_id='${PROJECT_ID}' and resource='volumes'"
+$SQL_CMD -e "update quota_usages set in_use=(select SUM(SIZE) from volumes where project_id='${PROJECT_ID}' and deleted=0) where project_id='${PROJECT_ID}' and resource='gigabytes'"
+
+echo "Should be fixed"
+COUNT=`$SQL_CMD -e "select COUNT(*) from volumes where deleted=0 and project_id='${PROJECT_ID}'"`
+echo "Actual volume usage is $COUNT"
+
+QUOTA_VOL=`$SQL_CMD -e "select in_use from quota_usages where resource='volumes' and project_id='${PROJECT_ID}'"`
+echo "Quota thinks it's $QUOTA_VOL"
